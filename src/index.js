c6301616ddcaf4fe91865d03651e33851e6f599f
abhishek-1802 <apuri_be20@thapar.edu>
2024-02-10 18:10:22 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const path = require("path")
+const express = require("express");
+const app = express();
+const port = 8000;
+
+
+const Register = require("../src/models/register");
+// const {json} = require("express")
+
+
+const staticPath =  path.join(__dirname, '../public')
+
+// app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(staticPath))
+
+app.get("/", (req,res) => {
+    res.send("hello form the server");
+})
+
+
+app.post("/", async (req,res) => {
+    try {
+        const member = await Register.findOneAndUpdate(
+            { email: req.body.email }, // Search criteria
+            { $set: req.body }, // Update or insert data
+            { upsert: true, new: true } // Options: upsert - insert if not exists, new - return the modified document
+        );
+
+        const hogya = await member.save();
+        res.status(201).sendFile(path.join(__dirname, '../public/index.html'));
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`listening to port ${port}`)
+})
+
---
+++ src/models/register.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/queries", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+
+const userScheme = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    subject: {
+        type: String
+    },
+    message: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model("Register", userScheme);
